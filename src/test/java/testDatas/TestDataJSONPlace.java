5a673f95fdbd30024d4174ee3383198ad1a49b19
aliyuksel64 <ayuksel6400@gmail.com>
2023-11-18 22:36:53 +0300

    API-5

--- src/test/java/testDatas/TestDataJSONPlace.java
+++ src/test/java/testDatas/TestDataJSONPlace.java
@@ -3,34 +3,49 @@ package testDatas;
 import org.json.JSONObject;
 
 import java.security.PublicKey;
+import java.util.HashMap;
 
 public class TestDataJSONPlace {
 
     public int okStatusKodu = 200;
     public String contentType = "application/json; charset=utf-8";
-    public String connectionHeader= "keep-alive";
+    public String connectionHeader = "keep-alive";
 
-    public JSONObject expBodyOlusturJSON(){
+    public JSONObject expBodyOlusturJSON() {
         JSONObject expBody = new JSONObject();
-        expBody.put("userId",3);
-        expBody.put("id",22);
-        expBody.put("title","dolor sint quo a velit explicabo quia nam");
-        expBody.put("body","eos qui et ipsum ipsam suscipit aut" +
+        expBody.put("userId", 3);
+        expBody.put("id", 22);
+        expBody.put("title", "dolor sint quo a velit explicabo quia nam");
+        expBody.put("body", "eos qui et ipsum ipsam suscipit aut" +
                 "\nsed omnis non odio" +
                 "\nexpedita earum mollitia molestiae aut atque rem suscipit" +
                 "\nnam impedit esse");
         return expBody;
     }
 
-    public JSONObject reqDataOlusturJSON(){
+    public JSONObject reqDataOlusturJSON() {
 
         JSONObject reqBody = new JSONObject();
-        reqBody.put("title","Ali");
-        reqBody.put("body","Merhaba");
-        reqBody.put("userId",10);
-        reqBody.put("id",70);
+        reqBody.put("title", "Ali");
+        reqBody.put("body", "Merhaba");
+        reqBody.put("userId", 10);
+        reqBody.put("id", 70);
 
         return reqBody;
+    }
+
+    public HashMap<String, Object> requestBodyOlusturMAP() {
+
+        HashMap<String,Object>reqBody=new HashMap<>();
+
+        reqBody.put("title", "Ali");
+        reqBody.put("body", "Merhaba");
+        reqBody.put("userId", 10.0);
+        reqBody.put("id", 70.0);
+
+        return reqBody;
+
+
     }
 
 
